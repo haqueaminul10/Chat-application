d7a28c36698c5e3f91fd5ae719f1436aef0da16c
haqueaminul10 <115152893+haqueaminul10@users.noreply.github.com>
2025-03-10 14:00:01 +0600

    connection establist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+
+const SOCKET_URL = 'http://localhost:3000';
 
 function App() {
-  return <>hellow</>;
+  useEffect(() => {
+    const socket = io(SOCKET_URL, { transports: ['websocket'] });
+
+    socket.on('connect', () => {
+      console.log('connected to server');
+    });
+
+    socket.on('message', (data) => {
+      console.log('Message from server:', data);
+    });
+
+    return () => {
+      socket.off('message');
+      socket.disconnect();
+      console.log('Disconnected from server');
+    };
+  }, []);
+
+  return <>Hello</>;
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -7,12 +7,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
   },
 });
-
-app.use(cors());
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
